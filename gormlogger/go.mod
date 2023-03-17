6f5008614fc4bd519876f3a39feaa1ef7b2a816f
kk <kk@hellotalk.com>
2023-03-17 14:47:24 +0800

    update

--- gormlogger/go.mod
+++ gormlogger/go.mod
@@ -1,16 +1,26 @@
-module code.hellotalk.com/infra/logger/gormlogger/v2
+module github.com/kk-kwok/logger/gormlogger
 
-go 1.17
+go 1.20
 
-replace code.hellotalk.com/infra/logger/v2 => ../
+replace github.com/kk-kwok/logger => ../
 
 require (
-	code.hellotalk.com/infra/logger/v2 v2.0.7
-	gorm.io/gorm v1.23.8
+	github.com/kk-kwok/logger v1.0.0
+	gorm.io/gorm v1.24.6
 )
 
 require (
+	github.com/labstack/echo/v4 v4.10.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
--- gormlogger/gorm.go
+++ gormlogger/gorm.go
@@ -1,87 +1,87 @@
 package gormlogger
 
 import (
-	"context"
-	"fmt"
-	"time"
+    "context"
+    "fmt"
+    "time"
 
-	"code.hellotalk.com/infra/logger/v2"
-	gormlogger "gorm.io/gorm/logger"
-	"gorm.io/gorm/utils"
+    "github.com/kk-kwok/logger"
+    gormlogger "gorm.io/gorm/logger"
+    "gorm.io/gorm/utils"
 )
 
 type gormLoggerImpl struct {
-	ctx           context.Context
-	log           logger.Logger
-	logLvl        gormlogger.LogLevel
-	slowThreshold time.Duration
+    ctx           context.Context
+    log           logger.Logger
+    logLvl        gormlogger.LogLevel
+    slowThreshold time.Duration
 }
 
 func NewDefault(l logger.Logger) gormlogger.Interface {
-	return NewGormLogger(context.Background(), l, 5*time.Second)
+    return NewGormLogger(context.Background(), l, 5*time.Second)
 }
 
 // Logger return singleton logger
 func NewGormLogger(ctx context.Context, log logger.Logger, slowThreshold time.Duration) gormlogger.Interface {
-	return &gormLoggerImpl{
-		ctx:           ctx,
-		log:           log,
-		logLvl:        gormlogger.Info,
-		slowThreshold: slowThreshold,
-	}
+    return &gormLoggerImpl{
+        ctx:           ctx,
+        log:           log,
+        logLvl:        gormlogger.Info,
+        slowThreshold: slowThreshold,
+    }
 }
 
 func (l *gormLoggerImpl) LogMode(lvl gormlogger.LogLevel) gormlogger.Interface {
-	l.logLvl = lvl
-	// l.log.SetLevel(toLoggerLevel(lvl))
-	return l
+    l.logLvl = lvl
+    // l.log.SetLevel(toLoggerLevel(lvl))
+    return l
 }
 
 func (l *gormLoggerImpl) Info(ctx context.Context, format string, args ...interface{}) {
-	l.log.Infof(format, args...)
+    l.log.Infof(format, args...)
 }
 
 func (l *gormLoggerImpl) Warn(ctx context.Context, format string, args ...interface{}) {
-	l.log.Warnf(format, args...)
+    l.log.Warnf(format, args...)
 }
 
 func (l *gormLoggerImpl) Error(ctx context.Context, format string, args ...interface{}) {
-	l.log.Errorf(format, args...)
+    l.log.Errorf(format, args...)
 }
 
 var (
-	traceStr     = "%s\n[%.3fms] [rows:%v] %s"
-	traceWarnStr = "%s %s\n[%.3fms] [rows:%v] %s"
-	traceErrStr  = "%s %s\n[%.3fms] [rows:%v] %s"
+    traceStr     = "%s\n[%.3fms] [rows:%v] %s"
+    traceWarnStr = "%s %s\n[%.3fms] [rows:%v] %s"
+    traceErrStr  = "%s %s\n[%.3fms] [rows:%v] %s"
 )
 
 // Trace print sql message
 func (l *gormLoggerImpl) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
-	if l.logLvl > gormlogger.Silent {
-		elapsed := time.Since(begin)
-		switch {
-		case err != nil && l.logLvl >= gormlogger.Error:
-			sql, rows := fc()
-			if rows == -1 {
-				l.log.Errorf(traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, "-", sql)
-			} else {
-				l.log.Errorf(traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
-			}
-		case elapsed > l.slowThreshold && l.slowThreshold != 0 && l.logLvl >= gormlogger.Warn:
-			sql, rows := fc()
-			slowLog := fmt.Sprintf("SLOW SQL >= %v", l.slowThreshold)
-			if rows == -1 {
-				l.log.Warnf(traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, "-", sql)
-			} else {
-				l.log.Warnf(traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, rows, sql)
-			}
-		case l.logLvl == gormlogger.Info:
-			sql, rows := fc()
-			if rows == -1 {
-				l.log.Infof(traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, "-", sql)
-			} else {
-				l.log.Infof(traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, rows, sql)
-			}
-		}
-	}
+    if l.logLvl > gormlogger.Silent {
+        elapsed := time.Since(begin)
+        switch {
+        case err != nil && l.logLvl >= gormlogger.Error:
+            sql, rows := fc()
+            if rows == -1 {
+                l.log.Errorf(traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+            } else {
+                l.log.Errorf(traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+            }
+        case elapsed > l.slowThreshold && l.slowThreshold != 0 && l.logLvl >= gormlogger.Warn:
+            sql, rows := fc()
+            slowLog := fmt.Sprintf("SLOW SQL >= %v", l.slowThreshold)
+            if rows == -1 {
+                l.log.Warnf(traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+            } else {
+                l.log.Warnf(traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+            }
+        case l.logLvl == gormlogger.Info:
+            sql, rows := fc()
+            if rows == -1 {
+                l.log.Infof(traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, "-", sql)
+            } else {
+                l.log.Infof(traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, rows, sql)
+            }
+        }
+    }
 }
